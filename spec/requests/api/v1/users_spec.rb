4de4ea79cc3eed4903c5e355dd8af84b5bb4e186
mark <marks88martinez@gmail.com>
2017-09-01 23:56:16 -0400

    addign create action to the user contraller with tests

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe 'Users API', type: :request do
       headers = {'Accept' => 'application/vnd.taskmanager.v1'}
       get "/users/#{user_id}", params: {}, headers: headers
     end
-
     context 'when the user exists' do
       it 'returns the user' do
         user_response = JSON.parse(response.body)
@@ -29,6 +28,41 @@ RSpec.describe 'Users API', type: :request do
       end
 
     end
+  end
+
+
+
+
+  describe 'POST /users' do
+
+    before do
+      headers = {'Accept' => 'application/vnd.taskmanager.v1'}
+      post '/users', params: {user: user_params}, headers: headers
+    end
+    context 'when the request params are valid' do
+
+      let(:user_params){attributes_for(:user)}
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+
+      end
+      it "returns json data for the created user" do
+        user_response = JSON.parse(response.body)
+        expect(user_response['email']).to eq(user_params[:email])
+
+      end
+
+    end
+    context "when the request params are invalid" do
+      let(:user_params){attributes_for(:user, email: 'marks@')}
+      it "returns status code 422" do
+        expect(response).to have_http_status(422)
+        user_response = JSON.parse(response.body)
+        expect(user_response).to have_key('errors')
+      end
+    end
+
 
   end
 end
