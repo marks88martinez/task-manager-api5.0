b1360e7041300a350b248df1d5e068d0387d8934
mark <marks88martinez@gmail.com>
2017-09-13 05:59:29 -0400

    Adding #authenticate_with_token! method

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,5 +1,9 @@
 module Authenticable
   def current_user
-    User.find_by(auth_token: request.headers['Authorization'])
+    @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
+  end
+
+  def authenticate_with_token!
+    render json: {errors: 'Unauthorized access!'}, status: 401 unless current_user.present?
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,7 +42,7 @@ RSpec.configure do |config|
     end
   end
     config.include FactoryGirl::Syntax::Methods
-    config.include RequestSpecHelper, type: :request
+    config.include RequestSpecHelper
 
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
