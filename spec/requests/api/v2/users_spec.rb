1b39810f111c1a98c27986ab027780e6d0f87a30
mark <marks88martinez@gmail.com>
2017-09-29 11:29:13 -0400

    Implementing JSONAPI.ORG patterns for users

--- spec/requests/api/v2/users_spec.rb
+++ spec/requests/api/v2/users_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe 'Users API', type: :request do
     end
     context 'when the user exists' do
       it 'returns the user' do
-        expect(json_body[:id]).to eq(user_id)
+        expect(json_body[:data][:id].to_i).to eq(user_id)
       end
 
       it 'returns status code 200' do
@@ -52,7 +52,7 @@ RSpec.describe 'Users API', type: :request do
 
       end
       it "returns json data for the created user" do
-        expect(json_body[:email]).to eq(user_params[:email])
+        expect(json_body[:data][:attributes][:email]).to eq(user_params[:email])
 
       end
 
@@ -87,7 +87,7 @@ RSpec.describe 'Users API', type: :request do
 
       end
       it "returns json data for the update user" do
-        expect(json_body[:email]).to eq(user_params[:email])
+        expect(json_body[:data][:attributes][:email]).to eq(user_params[:email])
       end
 
     end
